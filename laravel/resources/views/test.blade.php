b645da75a89c63e7f3b26629f7e5f383796b824f
Earl Savadera <earl_savadera@yahoo.com>
2016-11-23 21:39:28 +0800

    Update Survey Dashboard layout(with sidebar)

--- laravel/resources/views/test.blade.php
+++ laravel/resources/views/test.blade.php
@@ -75,6 +75,7 @@
                     <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
                     <li><a href="#">Layout</a></li>
                     <li class="active">Top Navigation</li>
+
                 </ol>
             </section>
             <section class="content">
